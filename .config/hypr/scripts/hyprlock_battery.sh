810984b39f1b610d2fea7af9bdcc450601721d9b
VisdethSara <visdethlatgehr@gmail.com>
2026-03-22 12:10:44 -0700

    Fix battery script, add credit

--- .config/hypr/scripts/hyprlock_battery.sh
+++ .config/hypr/scripts/hyprlock_battery.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-
+# --- SCRIPT IS A MODIFIED VERSION OF https://github.com/ashish-kus/dotfiles/blob/main/hypr/.config/hypr/bin/infonlock.sh
 # Get the current battery percentage
-battery_percentage=$(cat /sys/class/power_supply/BAT1/capacity)
+BATT_PERCENT=$(cat /sys/class/power_supply/BAT1/capacity)
 
 # Get the battery status (Charging or Discharging)
-battery_status=$(cat /sys/class/power_supply/BAT1/status)
+BATT_STATUS=$(cat /sys/class/power_supply/BAT1/status)
 
 # Define the battery icons for each 10% segment
-battery_icons=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹")
+ICONS=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹")
 
 # Define the charging icon
 charging_icon="󰂄"
 
 # Calculate index (0–8)
-INDEX=$(( PERCENT * (${#ICONS[@]} - 1) / 100 ))
+INDEX=$(( BATT_PERCENT * (${#ICONS[@]} - 1) / 100 ))
 
 ICON="${ICONS[$INDEX]}"
 
-
 # Check if the battery is charging
-if [ "$battery_status" = "Charging" ]; then
+if [ "$BATT_STATUS" = "Charging" ]; then
 	ICON="$charging_icon"
 fi
 
 # Output the battery percentage and icon
-echo "$battery_percentage% $ICON"
+echo "$BATT_PERCENT% $ICON"
